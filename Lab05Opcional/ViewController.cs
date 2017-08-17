33504cbb63d4c5c64872720ea41674fccff46e32
Manuel Marín <birrein@gmail.com>
2017-08-17 00:51:12 -0300

    Solución que cumple el objetivo
    Sin embargo, se podría modificar para que no se haga 10 veces el evento TouchUpInside por cada botón si no que sólo una vez.
    También modificar para poner los botones mediante layout para que se acomoden los botones dinamicamente.

---
+++ Lab05Opcional/ViewController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using UIKit;
+
+namespace Lab05Opcional
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            NumberOne.TouchUpInside += (object sender, EventArgs e) => 
+            {
+                ResultText.Text = ResultText.Text+"1";
+            };
+			NumberTwo.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "2";
+			};
+			NumberThree.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "3";
+			};
+			NumberFour.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "4";
+			};
+			NumberFive.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "5";
+			};
+			NumberSix.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "6";
+			};
+			NumberSeven.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "7";
+			};
+			NumberEight.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "8";
+			};
+			NumberNine.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "9";
+			};
+			NumberZero.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				ResultText.Text = ResultText.Text + "0";
+			};
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Lab05Opcional/ViewController.designer.cs
@@ -0,0 +1,118 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Lab05Opcional
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberEight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberFive { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberFour { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberNine { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberOne { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberSeven { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberSix { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberThree { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberTwo { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NumberZero { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ResultText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (NumberEight != null) {
+                NumberEight.Dispose ();
+                NumberEight = null;
+            }
+
+            if (NumberFive != null) {
+                NumberFive.Dispose ();
+                NumberFive = null;
+            }
+
+            if (NumberFour != null) {
+                NumberFour.Dispose ();
+                NumberFour = null;
+            }
+
+            if (NumberNine != null) {
+                NumberNine.Dispose ();
+                NumberNine = null;
+            }
+
+            if (NumberOne != null) {
+                NumberOne.Dispose ();
+                NumberOne = null;
+            }
+
+            if (NumberSeven != null) {
+                NumberSeven.Dispose ();
+                NumberSeven = null;
+            }
+
+            if (NumberSix != null) {
+                NumberSix.Dispose ();
+                NumberSix = null;
+            }
+
+            if (NumberThree != null) {
+                NumberThree.Dispose ();
+                NumberThree = null;
+            }
+
+            if (NumberTwo != null) {
+                NumberTwo.Dispose ();
+                NumberTwo = null;
+            }
+
+            if (NumberZero != null) {
+                NumberZero.Dispose ();
+                NumberZero = null;
+            }
+
+            if (ResultText != null) {
+                ResultText.Dispose ();
+                ResultText = null;
+            }
+        }
+    }
+}
